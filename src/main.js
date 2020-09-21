fd4b115730d02111ca600a84549aa1654f4be870
yulong <yulong@huayun.com>
2020-09-21 14:49:22 +0800

    chore: 使用vue2.0

--- src/main.js
+++ src/main.js
@@ -1,20 +1,26 @@
-import Vue, { createApp } from 'vue';
+import Vue from 'vue';
 import * as Sentry from "@sentry/browser";
 import { Vue as VueIntegration } from "@sentry/integrations";
 import { Integrations } from '@sentry/tracing';
 import App from './App.vue';
 
-Sentry.init({
+const options = {
   dsn: "http://641b545085f24809a3e2ca6a0c65b14e@localhost:9000/3",
   integrations: [
     new VueIntegration({
       Vue,
       tracing: true,
+      attachProps: true,
+      logErrors: true
     }),
     new Integrations.BrowserTracing(),
   ],
   tracesSampleRate: 1,
   release: 'pro@1.0.1',
-});
+}
 
-createApp(App).mount('#app');
+process.env.NODE_ENV === 'production' && Sentry.init(options);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
